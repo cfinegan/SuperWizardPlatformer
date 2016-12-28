7114f635a90224d87c01802eb695842f103a5351
Conor <conor_finegan@student.uml.edu>
2016-12-28 11:46:48 -0500

    Allowed binding of 2 keys to the same action.

--- SuperWizardPlatformer/InputMapper.cs
+++ SuperWizardPlatformer/InputMapper.cs
@@ -9,29 +9,38 @@ namespace SuperWizardPlatformer
         private static readonly int NUM_ACTIONS =
             Enum.GetValues(typeof(UserAction)).Cast<int>().Max() + 1;
 
-        private static Keys[] keyMappings = new Keys[NUM_ACTIONS];
+        private static KeyPair[] keyMappings = new KeyPair[NUM_ACTIONS];
 
         private static GamePadButtons[] padMappings = new GamePadButtons[NUM_ACTIONS];
 
         public static void Initialize()
-        {           
+        {
             // Gameplay key mappings.
             // TODO: Make configurable by user.
-            keyMappings[(int)UserAction.None] = Keys.None;
-            keyMappings[(int)UserAction.MoveLeft] = Keys.Left;
-            keyMappings[(int)UserAction.MoveRight] = Keys.Right;
-            keyMappings[(int)UserAction.Jump] = Keys.Up;
-            keyMappings[(int)UserAction.Duck] = Keys.Down;
+            keyMappings[(int)UserAction.None] = KeyPair.Empty;
+            keyMappings[(int)UserAction.MoveLeft] = new KeyPair(Keys.Left);
+            keyMappings[(int)UserAction.MoveRight] = new KeyPair(Keys.Right);
+            keyMappings[(int)UserAction.Jump] = new KeyPair(Keys.Up);
+            keyMappings[(int)UserAction.Duck] = new KeyPair(Keys.Down);
 
+            AddMenuKeyMappings();
+            AddGamePadMappings();
+        }
+
+        private static void AddMenuKeyMappings()
+        {
             // Menu key mappings.
             // These should stay hard-coded so that users can't break the menus.
-            keyMappings[(int)UserAction.MenuDown] = Keys.Down;
-            keyMappings[(int)UserAction.MenuUp] = Keys.Up;
-            keyMappings[(int)UserAction.MenuLeft] = Keys.Left;
-            keyMappings[(int)UserAction.MenuRight] = Keys.Right;
-            keyMappings[(int)UserAction.MenuAccept] = Keys.Enter;
-            keyMappings[(int)UserAction.MenuCancel] = Keys.Escape;
+            keyMappings[(int)UserAction.MenuDown] = new KeyPair(Keys.Down);
+            keyMappings[(int)UserAction.MenuUp] = new KeyPair(Keys.Up);
+            keyMappings[(int)UserAction.MenuLeft] = new KeyPair(Keys.Left);
+            keyMappings[(int)UserAction.MenuRight] = new KeyPair(Keys.Right);
+            keyMappings[(int)UserAction.MenuAccept] = new KeyPair(Keys.Enter, Keys.Space);
+            keyMappings[(int)UserAction.MenuCancel] = new KeyPair(Keys.Escape);
+        }
 
+        private static void AddGamePadMappings()
+        {
             // GamePad button mappings.
             // These can be hard-coded for now, customizable gamepad input would be nice.
             padMappings[(int)UserAction.MoveLeft] = GamePadButtons.Left;
@@ -39,7 +48,7 @@ namespace SuperWizardPlatformer
             padMappings[(int)UserAction.Duck] = GamePadButtons.Down;
             padMappings[(int)UserAction.Jump] = GamePadButtons.A;
 
-            // GamePad menu button mappings.
+            // GamePad menu button mappings, should stay hard-coded.
             padMappings[(int)UserAction.MenuUp] = GamePadButtons.Up;
             padMappings[(int)UserAction.MenuDown] = GamePadButtons.Down;
             padMappings[(int)UserAction.MenuLeft] = GamePadButtons.Left;
@@ -50,19 +59,22 @@ namespace SuperWizardPlatformer
 
         public static bool IsPressed(UserAction action)
         {
-            return KeyStateTracker.IsPressed(keyMappings[(int)action]) || 
+            return KeyStateTracker.IsPressed(keyMappings[(int)action].first) || 
+                KeyStateTracker.IsPressed(keyMappings[(int)action].second) ||
                 GamePadStateTracker.IsPressed(padMappings[(int)action]);
         }
 
         public static bool JustPressed(UserAction action)
         {
-            return KeyStateTracker.JustPressed(keyMappings[(int)action]) ||
+            return KeyStateTracker.JustPressed(keyMappings[(int)action].first) ||
+                KeyStateTracker.JustPressed(keyMappings[(int)action].second) ||
                 GamePadStateTracker.JustPressed(padMappings[(int)action]);
         }
 
         public static bool JustReleased(UserAction action)
         {
-            return KeyStateTracker.JustReleased(keyMappings[(int)action]) ||
+            return KeyStateTracker.JustReleased(keyMappings[(int)action].first) ||
+                KeyStateTracker.JustReleased(keyMappings[(int)action].second) ||
                 GamePadStateTracker.JustReleased(padMappings[(int)action]);
         }
 
@@ -77,7 +89,7 @@ namespace SuperWizardPlatformer
                 this.second = second;
             }
 
-            internal static KeyPair Empty = new KeyPair
+            internal static readonly KeyPair Empty = new KeyPair
             {
                 first = Keys.None,
                 second = Keys.None
