cc19b01d2acb22a96396d1a425574b5b46c12b53
Conor <conor_finegan@student.uml.edu>
2017-01-07 01:00:13 -0500

    Added state switch to Player.

--- SuperWizardPlatformer/Player.cs
+++ SuperWizardPlatformer/Player.cs
@@ -9,38 +9,98 @@ namespace SuperWizardPlatformer
 {
     class Player : DrawableEntity
     {
+        private enum States
+        {
+            Standing,
+            Jumping,
+            Falling,
+            Walking
+        }
+
+        private const float WALK_IMPULSE = 0.0035f;
+        private const float JUMP_IMPULSE = 0.01f;
+        private const int JUMP_FRAMES = 15;
+
+        private States state = States.Standing;
+        private int jumpFramesRemaining = 0;
+
         public Player(World world, TiledObject obj, TextureRegion2D textureRegion)
             : base(world, obj, textureRegion)
         {
             Body.BodyType = BodyType.Dynamic;
         }
 
-        public override void Update(IScene scene, GameTime gameTime)
+        private void StandingUpdate()
         {
-            float xVel = 0, yVel = 0;
-            float velFactor = 0.0025f;
+            if (ActionMapper.JustPressed(UserAction.Jump))
+            {
+                Jump();
+            }
+        }
+
+        private void Jump()
+        {
+            jumpFramesRemaining = JUMP_FRAMES;
+            state = States.Jumping;
+            JumpingUpdate();
+        }
+
+        private void JumpingUpdate()
+        {
+            Body.ApplyLinearImpulse(new Vector2(0, -JUMP_IMPULSE));
+
+            jumpFramesRemaining -= 1;
+
+            if (jumpFramesRemaining <= 0 || !ActionMapper.IsPressed(UserAction.Jump))
+            {
+                state = States.Falling;
+            }
+        }
+
+        private void FallingUpdate()
+        {
+            if (Body.LinearVelocity.Y == 0)
+            {
+                state = States.Standing;
+            }
+        }
+
+        private void WalkingUpdate()
+        {
+            StandingUpdate();
+        }
+
+        public override void Update(IScene scene, GameTime gameTime)
+        { 
+            switch (state)
+            {
+                case States.Standing:
+                    StandingUpdate();
+                    break;
+                case States.Jumping:
+                    JumpingUpdate();
+                    break;
+                case States.Falling:
+                    FallingUpdate();
+                    break;
+                case States.Walking:
+                    WalkingUpdate();
+                    break;
+            }
+
+            float xVel = 0;
 
             if (ActionMapper.IsPressed(UserAction.MoveLeft))
             {
-                xVel -= velFactor;
+                xVel -= WALK_IMPULSE;
             }
 
             if (ActionMapper.IsPressed(UserAction.MoveRight))
             {
-                xVel += velFactor;
+                xVel += WALK_IMPULSE;
             }
 
-            if (ActionMapper.IsPressed(UserAction.Jump))
-            {
-                yVel -= velFactor * 5;
-            }
-
-            if (ActionMapper.IsPressed(UserAction.Duck))
-            {
-                yVel += velFactor * 5;
-            }
-
-            Body.ApplyLinearImpulse(new Vector2(xVel, yVel));
+            Body.ApplyLinearImpulse(new Vector2(xVel, 0));
         }
 
         public override bool OnCollision(IEntity other)
