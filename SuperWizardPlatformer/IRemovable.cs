37b26925c8e71230cfd808984fa1e43bb6794aa3
Conor <conor_finegan@student.uml.edu>
2017-01-15 13:39:50 -0500

    Added documentation to IRemovable.

--- SuperWizardPlatformer/IRemovable.cs
+++ SuperWizardPlatformer/IRemovable.cs
@@ -1,5 +1,10 @@
 ﻿namespace SuperWizardPlatformer
 {
+    /// <summary>
+    /// Represents an object which can be marked for removal, so it can be cleaned up by 
+    /// the class that owns it at a later time. This is distrinct from an IDisposable, which 
+    /// is in charge of destroying its own resources.
+    /// </summary>
     interface IRemovable
     {
         bool IsMarkedForRemoval { get; set; }
