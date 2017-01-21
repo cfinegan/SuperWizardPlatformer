5a0c710c6df96066a251599d648abe497c9ce68c
Conor <conor_finegan@student.uml.edu>
2017-01-21 11:07:55 -0500

    Added 'seealso' ref to EntityContainer in IRemovable documentation.

--- SuperWizardPlatformer/IRemovable.cs
+++ SuperWizardPlatformer/IRemovable.cs
@@ -4,6 +4,7 @@
     /// Represents an object which can be marked for removal, so it can be cleaned up by 
     /// the class that owns it at a later time. This is distrinct from an IDisposable, which 
     /// is in charge of destroying its own resources.
+    /// <seealso cref="EntityContainer"/> 
     /// </summary>
     interface IRemovable
     {
